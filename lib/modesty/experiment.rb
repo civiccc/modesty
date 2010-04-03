f5c1d436e8edf728449aa740eb4f69e4b6c03957
Jay Adkisson <jay@causes.com>
2010-04-03 00:11:26 -0700

    fleshed out experiments

--- lib/modesty/experiment.rb
+++ lib/modesty/experiment.rb
@@ -2,16 +2,21 @@ module Modesty
   class Experiment
     class Builder
       def method_missing(name, *args)
-        if Experiment::ATTRIBUTES.include? name
-          @exp.instance_variable_set("@#{name}", args[0])
+        if Experiment::ATTRIBUTES.include?(name) && args.count > 0
+          val = (args.count == 1) ? args[0] : args
+          @exp.instance_variable_set("@#{name}", val)
         else
-          super
+          @exp.send(name)
         end
       end
 
       def initialize(exp)
         @exp = exp
       end
+    end
+
+    def initialize(slug)
+      @slug = slug
     end
 
     ATTRIBUTES = [
@@ -23,7 +28,27 @@ module Modesty
     attr_reader :slug
   end
 
-  class << self
+  module ExperimentMethods
+    def add_experiment(exp)
+      @experiments ||= {}
+      raise "Experiment already defined!" if @experiments[exp.slug]
+      @experiments[exp.slug] = exp
+    end
 
+    def new_experiment(slug, &block)
+      exp = Experiment.new(slug)
+      yield Experiment::Builder.new(exp) if block
+      exp.alternatives.each do |a|
+        exp.metrics.each do |m|
+          Modesty.new_metric(m.slug/a.slug, m)
+        end
+      end
+      add_experiment(exp)
+      exp
+    end
+  end
+
+  class << self
+    include ExperimentMethods
   end
 end
