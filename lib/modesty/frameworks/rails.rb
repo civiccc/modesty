92301219b1e07592be64c7cba655fbb35559a574
Kevin Ball <kmball11@gmail.com>
2010-11-10 11:04:12 -0800

    Update to work with rails 3

--- lib/modesty/frameworks/rails.rb
+++ lib/modesty/frameworks/rails.rb
@@ -1,7 +1,21 @@
-Modesty.root = File.join(Rails.root, 'modesty')
-Modesty.config_path = File.join(Rails.root, 'config', 'modesty.yml')
-Modesty.environment = Rails.env
-
-Rails.configuration.after_initialize do
-  Modesty.load!
+if Rails.version.match('^3')
+  module Modesty
+    class Railtie < Rails::Railtie
+      initializer "modesty.initialize" do |app|
+        Modesty.root = File.join(Rails.root, 'modesty')
+        Modesty.config_path = File.join(Rails.root, 'config', 'modesty.yml')
+        Modesty.environment = Rails.env
+        Rails.configuration.after_initialize do
+          Modesty.load!
+        end
+      end
+    end
+  end
+else
+  Modesty.root = File.join(Rails.root, 'modesty')
+  Modesty.config_path = File.join(Rails.root, 'config', 'modesty.yml')
+  Modesty.environment = Rails.env
+  Rails.configuration.after_initialize do
+    Modesty.load!
+  end
 end
