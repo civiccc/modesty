9dab86cecbeccb3c09f8a6669b8402cba4929866
Kevin Ball <kmball11@gmail.com>
2010-11-10 11:02:30 -0800

    Update mock_redis for ruby 1.9

--- vendor/mock_redis/lib/mock_redis/hash.rb
+++ vendor/mock_redis/lib/mock_redis/hash.rb
@@ -2,56 +2,56 @@ class MockRedis
   module HashMethods
     def hset(key, hkey, val)
       case h = self.hash[key]
-      when nil ; self.hash[key] = {hkey => val.to_s}
-      when Hash ; h[hkey] = val.to_s
+      when nil then self.hash[key] = {hkey => val.to_s}
+      when Hash then h[hkey] = val.to_s
       else fail "Not a hash"
       end
     end
 
     def hget(key, hkey)
       case h = self.hash[key]
-      when nil ; nil
-      when Hash ; h[hkey]
+      when nil then nil
+      when Hash then h[hkey]
       else fail "Not a hash"
       end
     end
 
     def hincrby(key, hkey, val)
       case h = self.hash[key]
-      when nil ; self.hash[key] = {hkey => val.to_s}
-      when Hash ; h[hkey] = (h[hkey].to_i + val).to_s
+      when nil then self.hash[key] = {hkey => val.to_s}
+      when Hash then h[hkey] = (h[hkey].to_i + val).to_s
       else fail "Not a hash"
       end
     end
 
     def hgetall(key)
       case h = self.hash[key]
-      when nil ; {}
-      when Hash ; h
+      when nil then {}
+      when Hash then h
       else fail "Not a hash"
       end
     end
 
     def hkeys(key)
       case h = self.hash[key]
-      when nil ; []
-      when Hash ; h.keys
+      when nil then []
+      when Hash then h.keys
       else fail "Not a hash"
       end
     end
 
     def hvals(key)
       case h = self.hash[key]
-      when nil ; []
-      when Hash ; h.values
+      when nil then []
+      when Hash then h.values
       else fail "Not a hash"
       end
     end
 
     def hlen(key)
       case h = self.hash[key]
-      when nil ; 0
-      when Hash ; h.keys.count
+      when nil then 0
+      when Hash then h.keys.count
       else fail "Not a hash"
       end
     end
